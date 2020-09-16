80ab11389dea8a6574f740a61235ede1a2dab7ba
Ayange Felix <ayangefelix8@gmail.com>
2020-09-16 14:40:29 -0700

    add relationships on the models

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -4,8 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
 
 class Question extends Model
 {
     use HasFactory;
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function replies()
+    {
+        return $this->hasMany(Reply::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -19,6 +19,14 @@ class User extends Authenticatable
     use Notifiable;
     use TwoFactorAuthenticatable;
 
+
+    public function questions()
+    {
+        return $this->hasMany(Question::class);
+    }
+
+
+
     /**
      * The attributes that are mass assignable.
      *
