901d95bfac93e66321c7594fbce91231fc51ed14
Ayange Felix <ayangefelix8@gmail.com>
2020-10-02 22:09:04 -0700

    configure vue and vuetify and all AppHome component

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,24 @@
-require('./bootstrap');
+require("./bootstrap");
+
+window.Vue = require("vue");
+import Vuetify from "vuetify";
+import "vuetify/dist/vuetify.min.css";
+import VueRouter from "vue-router";
+
+Vue.use(Vuetify);
+Vue.use(VueRouter);
+
+import AppHome from "./components/AppHome.vue";
+
+const router = new VueRouter({
+    mode: "history",
+    VueRouter
+});
+
+const app = new Vue({
+    el: "#app",
+    components: {
+        AppHome
+    },
+    router
+});
---
+++ resources/js/components/AppHome.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>Welcome to Home</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
